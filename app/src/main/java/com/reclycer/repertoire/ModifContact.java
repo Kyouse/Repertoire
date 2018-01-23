4ea7ffc0184ea26c310ffb1f1cda50be6c435b54
Kyouse <geo.leton@gmail.com>
2018-01-23 23:01:01 +0100

    Add butterKnife dependencies

---
+++ app/src/main/java/com/reclycer/repertoire/ModifContact.java
@@ -0,0 +1,13 @@
+package com.reclycer.repertoire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modif_contact);
+    }
+}
